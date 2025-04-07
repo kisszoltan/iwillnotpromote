66212d1153ec4de40cedb8511cb871d410d5a2bc
kisszoltan <kiss.zoltan.040016@gmail.com>
2025-04-07 16:14:58 +0200

    Default locale is now "en" in legal pages

--- src/app/legal/cookies/page.tsx
+++ src/app/legal/cookies/page.tsx
@@ -8,7 +8,7 @@ export default async function CookiesPage({
   const { locale } = await params;
 
   try {
-    const Content = (await import(`./${locale ?? "ne"}.mdx`)).default;
+    const Content = (await import(`./${locale ?? "en"}.mdx`)).default;
 
     return (
       <section className="flex flex-col gap-4">
